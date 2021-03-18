c6fc1bb51c4064cc97e16cbfd7a417868ea9827e
Anh Tran <tranngocanh0910@gmail.com>
2021-03-17 23:40:56 -0700

    change button

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -18,21 +18,39 @@ class LoginComponent extends Component{
             username: 'in28minutes',
             password:  ''
         }
-        this.handleUsernameChange=this.handleUsernameChange.bind(this)
+        // this.handleUsernameChange=this.handleUsernameChange.bind(this)
+        // this.handlePasswordChange=this.handlePasswordChange.bind(this)
+        this.handleChange=this.handleChange.bind(this)
     }
 
 
-    handleUsernameChange(event){
-        console.log(event);
+    handleChange(event){
+        console.log(this.state);
+        this.setState(
+            {
+                [event.target.name]
+                :event.target.value
+            }
+        )
     }
 
+    // handleUsernameChange(event){
+    //     console.log(event.target.value);
+    //     this.setState({username:event.target.value})
+    // }
+
+    // handlePasswordChange(event){
+    //     console.log(event.target.value);
+    //     this.setState({password:event.target.value})
+    // }
+    
     render(){
         return(
-            <>
-            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange}/>
-            Password: <input type="password" name="password" value={this.state.password}/>
+            <div>
+            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+            Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
             <button>Login</button>
-            </>
+            </div>
         )
     }
 }
